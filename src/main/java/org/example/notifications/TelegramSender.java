b475361417b71fa0bff27d43c7786bbcc8656e57
blountam <blountam@s21-student.21-school.ru>
2023-03-22 19:02:54 +0300

    first commit

---
+++ src/main/java/org/example/notifications/TelegramSender.java
@@ -0,0 +1,20 @@
+package org.example.notifications;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
+
+public final class TelegramSender implements SendAble {
+
+    private final TelegramBot bot;
+    private final long chatID;
+
+    public TelegramSender(String botToken, long chatID) {
+        this.bot = new TelegramBot(botToken);
+        this.chatID = chatID;
+    }
+
+    @Override
+    public void send(String message) {
+        bot.execute(new SendMessage(chatID, message));
+    }
+}
